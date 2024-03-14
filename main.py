bd1184f7aac0d11a17eb961b45f800d4e8595b54
Huynhngocuyen <139137335+hngocuyen@users.noreply.github.com>
2024-03-14 20:34:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import marshal,sys,os
-e = exec
-exec = print
-x = sys.argv[1]
-osn,oss=os.name,os.system
+ex,exec = exec,print
+x,osn,oss=sys.argv[1],os.name,os.system
 if osn == "nt":oss(f"COPY {x} temp_{x}")
 else:oss(f"cp {x} temp_{x}")
 __file__ = f"temp_{x}"
-e(marshal.loads(open(x,"rb").read()[16:]))
+ex(marshal.loads(open(x,"rb").read()[16:]))
 if osn == "nt":oss(f"del temp_{x}")
 else:oss(f"rm temp_{x}")
